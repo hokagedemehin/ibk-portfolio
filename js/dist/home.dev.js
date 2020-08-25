2bd4d5bc9b9fdbcf8c47fce232cbe8c6331363f7
Demehin Ibukunoluwa George <ibk2k7@gmail.com>
2020-08-25 15:53:13 +0100

    checking to see the size of card on laptop

---
+++ js/dist/home.dev.js
@@ -0,0 +1,32 @@
+"use strict";
+
+var header = document.querySelector('.nav-container'); // height = 
+
+window.addEventListener('scroll', function (e) {
+  e.preventDefault();
+  var height = window.pageYOffset;
+
+  if (height > 0) {
+    // console.log(height);
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
+var navToggle = document.querySelector('.nav-toggle');
+var linksContainer = document.querySelector('.links-container');
+var links = document.querySelector('.links');
+console.log(navToggle);
+navToggle.addEventListener('click', function () {
+  // linksContainer.classList.toggle('show-links')
+  var containerHeight = linksContainer.getBoundingClientRect().height;
+  var linksHeight = links.getBoundingClientRect().height;
+  console.log(linksHeight);
+  console.log(containerHeight);
+
+  if (containerHeight === 0) {
+    linksContainer.style.height = "".concat(linksHeight, "px");
+  } else {
+    linksContainer.style.height = 0;
+  }
+});
---
+++ js/home.js
@@ -0,0 +1,29 @@
+const header = document.querySelector('.nav-container');
+// height = 
+window.addEventListener('scroll', (e)=>{
+    e.preventDefault();
+    const height = window.pageYOffset;
+    if (height > 0){
+        // console.log(height);
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+const navToggle = document.querySelector('.nav-toggle');
+const linksContainer = document.querySelector('.links-container');
+const links = document.querySelector('.links');
+console.log(navToggle);
+navToggle.addEventListener('click', ()=>{
+    // linksContainer.classList.toggle('show-links')
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const linksHeight = links.getBoundingClientRect().height;
+    console.log(linksHeight);
+    console.log(containerHeight);
+    if(containerHeight === 0){
+        linksContainer.style.height = `${linksHeight}px`;
+    }
+    else{
+        linksContainer.style.height = 0;
+    }
+});
